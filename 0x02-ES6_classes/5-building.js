bfbe82f4bbb5c413d82da13dc0d8637d85389c7f
ITSNE01 <rabie.haddar1@gmail.com>
2025-04-17 11:48:45 +0000

    5-building.js

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,24 @@
+export default class Building {
+  constructor(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft must be a number');
+    }
+
+    this._sqft = sqft;
+
+    // Check if the method is overridden by the subclass
+    if (this.constructor !== Building && this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+
+  // Getter for sqft
+  get sqft() {
+    return this._sqft;
+  }
+
+  // Abstract method to be overridden
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
