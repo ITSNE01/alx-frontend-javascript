0df0becbc54367a1ddf85b9cd4ee58488eb9623b
ITSNE01 <rabie.haddar1@gmail.com>
2025-04-23 13:16:17 +0000

    task_6

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -47,6 +47,20 @@ function createEmployee(salary: number | string): Director | Teacher {
   return new Director();
 }
 
+function isDirector(employee: Director | Teacher): employee is Director {
+  return (employee as Director).workDirectorTasks !== undefined;
+}
+
+function executeWork(employee: Director | Teacher): string {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  }
+  return (employee as Teacher).workTeacherTasks();
+}
+
 console.log(createEmployee(200)); // Teacher
 console.log(createEmployee(1000)); // Director
 console.log(createEmployee('$500')); // Director
+
+console.log(executeWork(createEmployee(200))); // Getting to work
+console.log(executeWork(createEmployee(1000))); // Getting to director tasks
